05e22c874588e81b760f6c8c89ad7c83fa76e0dc
Anoop Singh Rawat <112460114+Anoop-Rawat@users.noreply.github.com>
2025-03-26 21:10:51 +0530

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,14 @@
+# provider.tf
+provider "aws" {
+  alias  = "dev"
+  region = "us-east-1"
+  access_key = var.dev_access_key
+  secret_key = var.dev_secret_key
+}
+
+provider "aws" {
+  alias  = "prod"
+  region = "us-east-1"
+  access_key = var.prod_access_key
+  secret_key = var.prod_secret_key
+}
