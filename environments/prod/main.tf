a65433fecba36dafe79281d71300e0628d63eddb
Anoop Singh Rawat <112460114+Anoop-Rawat@users.noreply.github.com>
2025-03-26 21:32:44 +0530

    Update main.tf

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -1,7 +1,16 @@
 # environments/prod/main.tf
+terraform {
+  backend "s3" {
+    bucket         = "rishika-terraform-state-bucket"
+    key            = "prod/terraform.tfstate"  # Unique key for prod
+    region         = "us-east-1"
+    
+  }
+}
+
 module "ec2_prod" {
   source        = "../../modules/ec2"
-  ami_id        = "ami-0c55b159cbfafe1f0"  # Replace with a valid AMI ID for us-east-1
+  ami_id        = "ami-0c55b159cbfafe1f0"  # Replace with valid AMI
   instance_type = "t2.micro"
   environment   = "prod"
   providers = {
