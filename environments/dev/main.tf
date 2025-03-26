92aa4244463d4c1267e54e29b3454fb3092cb5f3
Anoop Singh Rawat <112460114+Anoop-Rawat@users.noreply.github.com>
2025-03-26 22:01:00 +0530

    Update main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 module "ec2_dev" {
   source        = "../../modules/ec2"
-  ami_id        = "ami-0c55b159cbfafe1f0"  # Replace with a valid AMI ID for us-east-1
+  ami_id        = "ami-08b5b3a93ed654d19"  # Replace with a valid AMI ID for us-east-1
   instance_type = "t2.micro"
   environment   = "dev"
   providers = {
